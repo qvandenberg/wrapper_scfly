6f6ce97e6996216563a25f67ab05faa30499cbbb
Quincy van den Berg <quincy.vandenberg@physics.ox.ac.uk>
2017-12-09 04:19:50 +0000

    Pre holiday

--- new_scfly_analyse.py
+++ new_scfly_analyse.py
@@ -23,22 +23,22 @@ init = initFolderStructure(sys.argv[1])
 input_parameters = loadInput(sys.argv[1])
 
 ## Time integrate emission spectra
-# spec = timeIntSpec(input_parameters) # (input parameters, i_start, i_end, t_start, t_end) only "input parameters" is mandatory
-# spec.time_integrate(input_parameters)
-#
-# ## Apply f-scan. Build in synchronisation/lock threads. Make sure spectra are written out before executing this part.
-# supergauss_parameters = [0.957467, 3.54691, 0.46181, 0.042533, 0.1929, 0.21648] #
-# spec.fscan(supergauss_parameters)
+spec = timeIntSpec(input_parameters) # (input parameters, i_start, i_end, t_start, t_end) only "input parameters" is mandatory
+spec.time_integrate(input_parameters)
+
+## Apply f-scan. Build in synchronisation/lock threads. Make sure spectra are written out before executing this part.
+supergauss_parameters = [0.957467, 3.54691, 0.46181, 0.042533, 0.1929, 0.21648] #
+spec.fscan(supergauss_parameters)
 
 ## Smoothening of spectra
 # spec.broaden(np.linspace(1,20,20), 200, 'GAUSS') # (vector of folders, width [ev], lineshape)
 # spec.broaden('TOTAL', 200, 'GAUSS') # (vector of folders, width [ev], lineshape)
 
 ## Extract: temperature-density conditions, population, rates
-extract = extract(input_parameters)
-extract.temperature_density(input_parameters)
-extract.populations(input_parameters,[3,10],'single_ch')
-extract.rates(input_parameters,[3,10],'single_ch','coll_ion')
+# extract = extract(input_parameters)
+# extract.temperature_density(input_parameters)
+# extract.populations(input_parameters,[3,10],'single_ch')
+# extract.rates(input_parameters,[3,10],'single_ch','coll_ion')
 
 
 
